b26d552a41d4c07c4b932f4a4bff842cc8813236
misogi <dev@misogi.info>
2014-12-30 13:43:08 +0900

    init commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,10 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let frame = CGRect(x: 0, y: 0, width: 200, height: 100)
+let view = UILabel(frame: frame)
+view.backgroundColor = UIColor.lightGrayColor()
+view.textAlignment = .Center
+view.text = "Hello"
+view
